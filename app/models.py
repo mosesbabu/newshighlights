f53ddcfafc1532bd6caf3fd198de770d1a39c48e
mosesbabu <44118179+mosesbabu@users.noreply.github.com>
2020-04-28 09:28:23 +0300

    added source class to models

--- app/models.py
+++ app/models.py
@@ -2,13 +2,13 @@ class Article:
     '''
     Class that instantiates objects of the news article objects of the news sources
     '''
-    def __init__(self,author,description,time,url,image,title):
+    def __init__(self,author,description,time,url,title,image):
         self.author = author
         self.description = description
         self.time = time
         self.url = url
-        self.image = image
         self.title = title
+        self.image = image
 
 class Category:
     '''
