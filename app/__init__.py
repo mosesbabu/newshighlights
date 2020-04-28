ccc645efaa2eed4a12656721fd542cd39f0d513f
mosesbabu <44118179+mosesbabu@users.noreply.github.com>
2020-04-28 09:33:00 +0300

    updated __init.py__

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,11 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
-
+    
     # Initializing flask extensions
     bootstrap.init_app(app)
+    
+    
 
 # Registering the blueprint
     from .main import main as main_blueprint
