877561bc376e9f7f150ae3583ff4316bbe0b26f8
mosesbabu <44118179+mosesbabu@users.noreply.github.com>
2020-04-28 09:30:23 +0300

    updated the article tests

--- tests/test_article.py
+++ tests/test_article.py
@@ -8,7 +8,7 @@ class ArticleTest(unittest.TestCase):
 
     def setUp(self):
         '''
-        Set up method that will run before every Test
+        Set up method that to run before every Test
         '''
         self.new_article = Article('Naila-Jean Meyers and Scott Cacciola','In the 30th professional match between the sisters, Serena defeated Venus in the third round. Rafael Nadal, Juan Martin del Potro and Sloane Stephens also won Friday.','2018-09-01T05:22:26Z','https://www.nytimes.com/2018/08/31/sports/tennis/us-open-results.html','https://static01.nyt.com/images/2018/09/01/sports/01openlive7/01openlive7-facebookJumbo.jpg','US Open 2018 Results')
 
